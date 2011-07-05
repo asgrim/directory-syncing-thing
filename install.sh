71e04eadeffccdb4f8ab779cb52f51bcd4080016
James Titcumb <james@asgrim.com>
2011-07-05 09:17:06 +0100

    Added a very rudimentary install script and instructions

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (C) 2011 James Titcumb
+#
+# See LICENSE file for license!
+#
+
+cp directory-syncing-thing.conf.example /etc/directory-syncing-thing.conf
+ln -s /opt/directory-syncing-thing/directory-syncing-thing /etc/init.d/directory-syncing-thing
